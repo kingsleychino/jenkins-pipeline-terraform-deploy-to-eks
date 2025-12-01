2d856c5a8a82fe70c9b06cb1726d8304aa123e8d
Kingsley <kingsleychinono@gmail.com>
2025-12-01 01:45:01 +0100

    updated directory

--- terraform/eks-cluster.tf
+++ terraform/eks-cluster.tf
@@ -1,26 +1,44 @@
 module "eks" {
-    source  = "terraform-aws-modules/eks/aws"
-    version = "~> 19.0"
-    cluster_name = "myapp-eks-cluster"
-    cluster_version = "1.24"
-
-    cluster_endpoint_public_access  = true
-
-    vpc_id = module.myapp-vpc.vpc_id
-    subnet_ids = module.myapp-vpc.private_subnets
-
-    tags = {
-        environment = "development"
-        application = "myapp"
+  source  = "terraform-aws-modules/eks/aws"
+  version = "~> 20.0"
+  
+  cluster_name    = "myapp-eks-cluster"
+  cluster_version = "1.31"  # Use a supported version (1.28, 1.29, 1.30, or 1.31)
+  
+  cluster_endpoint_public_access = true
+  
+  # Required for module v20.x
+  enable_cluster_creator_admin_permissions = true
+  
+  vpc_id     = module.myapp-vpc.vpc_id
+  subnet_ids = module.myapp-vpc.private_subnets
+  
+  # Control plane subnet IDs (required in v20.x)
+  control_plane_subnet_ids = concat(
+    module.myapp-vpc.private_subnets,
+    module.myapp-vpc.public_subnets
+  )
+  
+  tags = {
+    environment = "development"
+    application = "myapp"
+  }
+  
+  eks_managed_node_groups = {
+    dev = {
+      min_size     = 1
+      max_size     = 3
+      desired_size = 2
+      
+      instance_types = ["t2.small"]
+      
+      # Additional recommended settings
+      capacity_type = "ON_DEMAND"
+      
+      labels = {
+        Environment = "development"
+        NodeGroup   = "dev"
+      }
     }
-
-    eks_managed_node_groups = {
-        dev = {
-            min_size = 1
-            max_size = 3
-            desired_size = 2
-
-            instance_types = ["t2.small"]
-        }
-    }
+  }
 }
--- terraform/vpc.tf
+++ terraform/vpc.tf
@@ -1,28 +1,41 @@
-data "aws_availability_zones" "azs" {}
+data "aws_availability_zones" "azs" {
+  state = "available"
+}
+
 module "myapp-vpc" {
-  source          = "terraform-aws-modules/vpc/aws"
-  version         = "~> 5.0"
-  name            = "myapp-vpc"
-  cidr            = var.vpc_cidr_block
+  source  = "terraform-aws-modules/vpc/aws"
+  version = "~> 5.0"
+  
+  name = "myapp-vpc"
+  cidr = var.vpc_cidr_block
+  
   private_subnets = var.private_subnet_cidr_blocks
   public_subnets  = var.public_subnet_cidr_blocks
   azs             = data.aws_availability_zones.azs.names
-
+  
   enable_nat_gateway   = true
   single_nat_gateway   = true
   enable_dns_hostnames = true
-
+  enable_dns_support   = true  # Add this
+  
+  # Enable these for EKS requirements
+  map_public_ip_on_launch = true  # For public subnets
+  
   tags = {
     "kubernetes.io/cluster/myapp-eks-cluster" = "shared"
+    Environment                                = "development"
+    Application                                = "myapp"
   }
-
+  
   public_subnet_tags = {
     "kubernetes.io/cluster/myapp-eks-cluster" = "shared"
-    "kubernetes.io/role/elb"                  = 1
+    "kubernetes.io/role/elb"                  = "1"  # Changed to string
+    Type                                      = "public"
   }
-
+  
   private_subnet_tags = {
     "kubernetes.io/cluster/myapp-eks-cluster" = "shared"
-    "kubernetes.io/role/internal-elb"         = 1
+    "kubernetes.io/role/internal-elb"         = "1"  # Changed to string
+    Type                                      = "private"
   }
 }
